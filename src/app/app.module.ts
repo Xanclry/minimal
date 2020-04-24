a408df19730875f57f39125bb5d8e4ad87e372bd
Your Name <fed_rekish@hotmail.com>
2020-04-24 19:37:38 +0300

    added animated tabs to banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './top-menu/top-menu.component';
 import { BannerComponent } from './banner/banner.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './banner/banner.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,7 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-banner',
+  animations: [
+    trigger('activeUnactive', [
+      // ...
+      state('active', style({
+        backgroundColor: '*'
+      })),
+      state('unactive', style({
+        backgroundColor: 'white'
+      })),
+      transition('* => *', [
+        animate(100)
+      ]),
+      transition('void <=> *', [
+        animate(0)
+      ])
+
+    ]),
+  ],
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.css']
 })
@@ -9,7 +35,16 @@ export class BannerComponent implements OnInit {
 
   constructor() { }
 
+  activeButton: number = 0
+
+  private colours = ['100', '185', '310']
+
   ngOnInit(): void {
+  }
+
+  changeColor(buttonNumber: number) {
+    this.activeButton = buttonNumber
+    document.documentElement.style.setProperty('--hue-start', this.colours[buttonNumber].toString())
   }
 
 }
